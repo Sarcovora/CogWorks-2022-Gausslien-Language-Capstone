3ebe046c9790d3cdc9ccbdae30c45cb55b4eb494
Elaine J <86863141+elainejiangg@users.noreply.github.com>
2022-07-28 17:30:37 -0400

    Create IDF.py

---
+++ OmniCog/IDF.py
@@ -0,0 +1,34 @@
+from collections import Counter
+import math
+from numpy import linalg
+
+bigCount = Counter()
+captions = 0
+
+for caption_info in coco_data["annotations"]:
+    captions +=1
+    caption_vocab = set(tokenize(caption_info["caption"]))
+    bigCount.update(caption_vocab)
+        
+def IDFs(counter):
+    """ 
+   
+    Parameters
+    ---------
+    counter : Iterable[collections.Counter]
+        An iterable containing {word -> count} counters for respective
+        captions.
+    
+    Returns
+    -------
+    IDFs: a dictionary mapping vocab words to inverse document frequencies
+    """
+    
+    
+    vocab = bigCount.keys()
+    IDFs = {}
+    for word in vocab:
+        IDFs[word] = math.log10(captions/bigCount[word])
+    
+    return IDFs
+  
