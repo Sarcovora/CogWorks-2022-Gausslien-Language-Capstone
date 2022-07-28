f532f451663c42441ba201051da0d9c80b91fdb7
Alan W <awangc31s@gmail.com>
2022-07-28 19:05:59 -0400

    init

--- OmniCog/__init__.py
+++ OmniCog/__init__.py
@@ -1,5 +1,6 @@
 # Imports for OmniCog
 
+from .manager import CocoDataManager
 from .loadCoco import load_coco
 from .loss_and_acc import *
-from .manager import CocoDataManager
+from .model_data_processing import *
