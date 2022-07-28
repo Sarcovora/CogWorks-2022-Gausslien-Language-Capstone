4985403c0f514cb611d1182a01002db295752177
Elaine J <86863141+elainejiangg@users.noreply.github.com>
2022-07-28 17:40:51 -0400

    added docstring

--- OmniCog/embed_query.py
+++ OmniCog/embed_query.py
@@ -4,6 +4,16 @@ def normalize(unnorm_embed):
     return embed/linalg.norm(embed, axis = 1)
   
 def query_embed(query):
+    """
+    Parameter:
+        query: string
+            User's input caption/query
+    
+    Returns:
+        embed: np.array(200,)
+            IDF-weighted sum of the glove-embedding for each word in the caption
+    
+    """
     embed = np.zeros(200,)
     tokens = tokenize(query)
     try:
