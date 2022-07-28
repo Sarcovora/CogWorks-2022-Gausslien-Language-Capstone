3f30bac2498d5eaf3805196a8e2d8daf2c59e39b
Alan W <awangc31s@gmail.com>
2022-07-28 18:08:31 -0400

    changed name to avoid package conflict

---
+++ OmniCog/tokenize_captions.py
@@ -0,0 +1,13 @@
+from collections import Counter
+import numpy as np
+import re, string
+
+punc_regex = re.compile('[{}]'.format(re.escape(string.punctuation)))
+
+def strip_punc(corpus):
+    return punc_regex.sub('', corpus)
+
+
+def tokenize_caption(caption):
+    caption = strip_punc(caption)
+    return caption.lower().split()
