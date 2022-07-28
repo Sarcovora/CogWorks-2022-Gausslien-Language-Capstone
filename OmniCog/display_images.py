e26119386e224277393507ee4ba2f09adc2640c1
caasib <107368103+caasib@users.noreply.github.com>
2022-07-28 17:21:50 -0400

    fixed display images cause i had a bruh moment

--- OmniCog/display_images.py
+++ OmniCog/display_images.py
@@ -1,3 +1,25 @@
+import io
+
+import requests
+from PIL import Image
+
+
+def download_image(img_url: str) -> Image:
+    """Fetches an image from the web.
+
+    Parameters
+    ----------
+    img_url : string
+        The url of the image to fetch.
+
+    Returns
+    -------
+    PIL.Image
+        The image."""
+
+    response = requests.get(img_url)
+    return Image.open(io.BytesIO(response.content))
+
 def display_images(k_similarity_scores):
     """Displays the images that were returned by the query_database function. (I think)
     
